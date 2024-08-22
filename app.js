64fa8da1dd9e0bac77f033ed7fdbfb033e230f5c
Faiz <faizkhan8225@gmail.com>
2024-08-22 13:41:20 +0530

    Understanding post request and body parser.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/currentime", (req, res, next) => {
     res.send(new Date().toISOString())
 })
 
-app.use((req, res, next) => {
-    res.status(200).send("<h1>Hello World</h1>")
+app.get("/", (req, res, next) => {
+    res.send('<form action="/store-user" method="POST"><label for="username">Your Name</label><input type="text" name="username" id="username"/><button>Submit</button></form>')
+})
+
+app.post('/store-user', (req, res, next) => {
+    console.log(req.body.username);
+    res.send("<h1>User stored</h1>")
 })
 
 app.listen(3000, () => console.log("Server start at port: 3000"))
