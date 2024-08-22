625d66ffef5119e02b366d4846192961f38d4fb7
Faiz <faizkhan8225@gmail.com>
2024-08-22 15:17:47 +0530

    Enhance nodejs code by using expressJs

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/currentime", (req, res, next) => {
-    res.send(new Date().toISOString())
-})
+  res.send(new Date().toISOString());
+});
 
 app.get("/", (req, res, next) => {
-    res.send('<form action="/store-user" method="POST"><label for="username">Your Name</label><input type="text" name="username" id="username"/><button>Submit</button></form>')
+  res.send(
+    '<form action="/store-user" method="POST"><label for="username">Your Name</label><input type="text" name="username" id="username"/><button>Submit</button></form>'
+  );
+});
+
+app.post("/store-user", (req, res, next) => {
+  const username = req.body.username;
+  const p = path.join(__dirname, "data", "store.json");
+
+    const fileData = fs.readFileSync(p);
+    const existingUser = JSON.parse(fileData);
+    existingUser.push({ id: Math.random(), name: username });
+    fs.writeFileSync(p, JSON.stringify(existingUser));
+  res.send("<h1>User stored</h1>");
+});
+
+app.get("/users", (req, res, next) => {
+    const p = path.join(__dirname, "data", "store.json")
+    const fileData = fs.readFileSync(p)
+    const users = JSON.parse(fileData)
+    const userList = users.map(user => `<li>${user.name}</li>`).join('');
+    res.send(`<ul>${userList}</ul>`)
 })
 
-app.post('/store-user', (req, res, next) => {
-    console.log(req.body.username);
-    res.send("<h1>User stored</h1>")
-})
-
-app.listen(3000, () => console.log("Server start at port: 3000"))
+app.listen(3000, () => console.log("Server start at port: 3000"));
